b9c6177fca10541bf893867fc1fef5be3f288cbd
Peter Winskill <p.winskill@imperial.ac.uk>
2021-12-15 14:45:35 +0000

    ÂOverhaul visuals with median post model

--- 03_visualise_outputs.R
+++ 03_visualise_outputs.R
@@ -16,7 +16,25 @@ paton <- readRDS("ignore/prob_hosp/paton_inferred.rds") %>%
   select(pfpr, sma, distance, act, py, country)
 
 ### Wrangle parameters #########################################################
-samples <- sample_chains(mcmc, 1000)
+samples <- sample_chains(mcmc, 100)
+
+global_parameters <- samples %>%
+  select(-contains("ccc_"), -contains("hosp"))
+
+country_parameters <- samples %>%
+  select(contains("ccc_"), sample) %>%
+  pivot_longer(-sample, names_to = "country", values_to = "country_capacity", names_prefix = "ccc_") 
+
+hospital_parameters <- samples %>%
+  select(contains("hosp_"), sample) %>%
+  pivot_longer(-sample, names_to = "country", values_to = "hosp", names_prefix = "hosp_") 
+
+parameters <- global_parameters %>%
+  left_join(country_parameters, by = "sample") %>%
+  left_join(hospital_parameters, by = c("country", "sample")) %>%
+  select(country, sample, everything())
+################################################################################
+
 
 global_parameters <- samples %>%
   select(-contains("ccc_"), -contains("hosp"))
@@ -41,27 +59,22 @@ country_parameters <- samples %>%
   summarise(country_capacity = median(country_capacity))
 
 
-#country_summary_par <- bind_rows(
-#  apply(country_parameters, 2, median),
-#  apply(country_parameters, 2, quantile, probs = 0.025),
-#  apply(country_parameters, 2, quantile, probs = 0.975)
-#) %>%
-#  mutate(sample = c("Median", "Lower CrI", "Upper CrI"))
-#country_parameters <- bind_rows(country_summary_par, mutate(country_parameters, sample = as.character(sample))) %>%
-#  pivot_longer(-sample, names_to = "country", values_to = "country_capacity", names_prefix = "ccc_")
+hospital_parameters <- samples %>%
+  select(contains("hosp_"), sample) %>%
+  pivot_longer(-sample, names_to = "country", values_to = "hosp", names_prefix = "hosp_") %>%
+  group_by(country) %>%
+  summarise(hosp = median(hosp))
 
 hospital_parameters <- samples %>%
   select(contains("hosp_"), sample) 
 hospital_summary_par <- bind_rows(
   apply(hospital_parameters, 2, median),
-  apply(hospital_parameters, 2, quantile, probs = 0.025),
-  apply(hospital_parameters, 2, quantile, probs = 0.975)
+  apply(hospital_parameters, 2, quantile, probs = 0.975),
+  apply(hospital_parameters, 2, quantile, probs = 0.025)
 ) %>%
   mutate(sample = c("Median", "Lower CrI", "Upper CrI"))
 hospital_parameters <- bind_rows(hospital_summary_par, mutate(hospital_parameters, sample = as.character(sample))) %>%
   pivot_longer(-sample, names_to = "country", values_to = "hosp", names_prefix = "hosp_")
-
-
 ################################################################################
 
 ### Prepare and summarise DHS data #############################################
@@ -76,7 +89,7 @@ dhs_var_summary <- dhs_sma %>%
 dhs_pfpr_summary <- dhs_sma %>%
   bind_rows() %>%
   group_by(country) %>%
-  mutate(pfpr_g = cut_number(pfpr, 7)) %>%
+  mutate(pfpr_g = cut_number(pfpr, 5)) %>%
   group_by(country, pfpr_g) %>%
   summarise(
     pfprl = quantile(pfpr, 0.025),
@@ -102,11 +115,11 @@ dhs_distance_summary <- dhs_sma %>%
 paton_var_summary <- paton %>%
   bind_rows() %>%
   group_by(country) %>%
-  summarise(mean_distance = mean(distance),
+  summarise(mean_distance = weighted.mean(distance, py),
             max_distance = max(distance),
-            mean_pfpr = mean(pfpr),
+            mean_pfpr = weighted.mean(pfpr, py),
             max_pfpr = max(pfpr),
-            mean_act = mean(act),
+            mean_act = weighted.mean(act, py),
             max_act = max(act))
 
 paton_summary <- paton %>%
@@ -117,13 +130,16 @@ paton_summary <- paton %>%
 
 ### Prediction data.frames #####################################################
 pfpr_df <- data.frame(pfpr = seq(0, 0.8, 0.01))
-distance_df <- data.frame(distance = 0:500)
+distance_df <- data.frame(distance = seq(0, 50, 0.1))
 
-dhs_pfpr_prediction <- country_parameters %>%
-  left_join(global_parameters, by = "sample") %>%
+dhs_pfpr_prediction <- parameters %>%
   left_join(pfpr_df, by = character()) %>%
+  # add mean distance for each country
   left_join(select(dhs_var_summary, country, mean_distance), by = "country")%>%
   rename(distance = mean_distance) %>%
+  # Filter PfPr outside of observed range
+  left_join(select(dhs_var_summary, country, max_pfpr), by = "country") %>%
+  filter(pfpr <= max_pfpr) %>%
   mutate(sma_prevalence = gompertz(pfpr = pfpr,
                                    distance = distance,
                                    global_capacity = global_capacity, 
@@ -131,11 +147,19 @@ dhs_pfpr_prediction <- country_parameters %>%
                                    distance_beta = distance_beta,
                                    pfpr_beta = pfpr_beta,
                                    shift = shift))
-dhs_distance_prediction <- country_parameters %>%
-  left_join(global_parameters, by = "sample") %>%
+
+dhs_pfpr_median_prediction <- dhs_pfpr_prediction %>%
+  group_by(country, pfpr) %>%
+  summarise(sma_prevalence = median(sma_prevalence))
+
+
+
+dhs_distance_prediction <- parameters %>%
   left_join(distance_df, by = character()) %>%
-  left_join(select(dhs_var_summary, country, mean_pfpr), by = "country")%>%
+  # add mean pfpr for each country
+  left_join(select(dhs_var_summary, country, mean_pfpr), by = "country") %>%
   rename(pfpr = mean_pfpr) %>%
+  # Filter distance outside of observed range
   left_join(select(dhs_var_summary, country, max_distance), by = "country") %>%
   filter(distance <= max_distance) %>%
   mutate(sma_prevalence = gompertz(pfpr = pfpr,
@@ -146,7 +170,11 @@ dhs_distance_prediction <- country_parameters %>%
                                    pfpr_beta = pfpr_beta,
                                    shift = shift))
 
-pfpr_prediction <- global_parameters %>%
+dhs_distance_median_prediction <- dhs_distance_prediction %>%
+  group_by(country, distance) %>%
+  summarise(sma_prevalence = median(sma_prevalence))
+
+pfpr_prediction <- parameters %>%
   mutate(country_capacity = 0,
          act = 0.5) %>%
   left_join(pfpr_df, by = character()) %>%
@@ -159,7 +187,12 @@ pfpr_prediction <- global_parameters %>%
                                    pfpr_beta = pfpr_beta,
                                    shift = shift))
 
-distance_prediction <- global_parameters %>%
+pfpr_median_prediction <- pfpr_prediction %>%
+  group_by(pfpr) %>%
+  summarise(sma_prevalence = median(sma_prevalence))
+
+
+distance_prediction <- parameters %>%
   mutate(country_capacity = 0) %>%
   left_join(distance_df, by = character()) %>%
   mutate(pfpr = mean(dhs_var_summary$mean_pfpr)) %>%
@@ -171,11 +204,16 @@ distance_prediction <- global_parameters %>%
                                    pfpr_beta = pfpr_beta,
                                    shift = shift))
 
-paton_prediction <- hospital_parameters %>%
-  left_join(country_parameters, by = c("sample", "country")) %>%
-  left_join(global_parameters, by = "sample") %>%
+distance_median_prediction <- distance_prediction %>%
+  group_by(distance) %>%
+  summarise(sma_prevalence = median(sma_prevalence))
+
+
+
+paton_prediction <- parameters %>%
+  filter(country %in% paton_var_summary$country) %>%
   left_join(pfpr_df, by = character()) %>%
-  left_join(select(paton_var_summary, country, mean_distance, mean_act), by = "country")%>%
+  left_join(select(paton_var_summary, country, mean_distance, mean_act), by = "country") %>%
   rename(distance = mean_distance,
          act = mean_act,
     ) %>%
@@ -196,10 +234,19 @@ paton_prediction <- hospital_parameters %>%
          hospital = community / hosp,
          p_hosp = 1 - p(hosp))
 
+paton_median_prediction <- paton_prediction %>%
+  group_by(country, pfpr) %>%
+  summarise(community = median(community),
+            hospital = median(hospital),
+            p_hosp = median(p_hosp))
+
 hospital_comunity <- paton_prediction %>%
-  select(country, pfpr, sample, alpha, col, lty, community, hospital) %>%
-  pivot_longer(c(community, hospital), names_to = "location", values_to = "inc") %>%
-  filter(sample %in% c("Median", "Lower CrI", "Upper CrI"))
+  select(country, pfpr, sample, community, hospital) %>%
+  pivot_longer(c(community, hospital), names_to = "location", values_to = "inc")
+
+hospital_community_median <- hospital_comunity %>%
+  group_by(country, pfpr, location) %>%
+  summarise(inc = median(inc))
 
 paton_model_prediction <- 
   data.frame(country = c("Kenya", "Tanzania", "Uganda")) %>%
@@ -210,51 +257,41 @@ paton_model_prediction <-
 
 ### DHS fit plots ##############################################################
 dhs_pfpr_fits <- ggplot() +
-    geom_line(data = dhs_pfpr_prediction, aes(x = pfpr, y = sma_prevalence, group = sample,
-                                            alpha = alpha, col = col, lty = lty)) +
-  geom_linerange(data = dhs_pfpr_summary, aes(x = pfpr, ymin = smal, ymax = smau), col = "dodgerblue") +
-  geom_linerange(data = dhs_pfpr_summary, aes(y = sma_prevalence, xmin = pfprl, xmax = pfpru), col = "dodgerblue") +
-  geom_point(data = dhs_pfpr_summary, aes(x = pfpr, y = sma_prevalence), col = "dodgerblue") +
-  scale_linetype_manual(values = c(1, 2, 1), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_colour_manual(values = c("red", "darkred", "black"), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_alpha(guide = "none") + 
-  xlab(expression(~italic(Pf)~Pr[2-10])) +
-  ylab(expression(SMAPr[0.5-5])) + 
-  facet_wrap(~ country, scales = "free_y") +
-  theme_bw()
-
-dhs_distance_fits <- ggplot() +
-  geom_line(data = dhs_distance_prediction, aes(x = distance, y = sma_prevalence, group = sample,
-                                            alpha = alpha, col = col, lty = lty)) +
-  geom_linerange(data = dhs_distance_summary, aes(x = distance, ymin = smal, ymax = smau), col = "dodgerblue") +
-  geom_linerange(data = dhs_distance_summary, aes(y = sma_prevalence, xmin = distancel, xmax = distanceu), col = "dodgerblue") +
-  geom_point(data = dhs_distance_summary, aes(x = distance, y = sma_prevalence), col = "dodgerblue") +
-  scale_linetype_manual(values = c(1, 2, 1), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_colour_manual(values = c("red", "darkred", "black"), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_alpha(guide = "none") + 
+  geom_line(data = dhs_pfpr_prediction, aes(x = pfpr, y = sma_prevalence, group = sample), alpha = 0.1, col = "#00798c") +
+  geom_line(data = dhs_pfpr_median_prediction, aes(x = pfpr, y = sma_prevalence), col = "#edae49", size = 1) +
+  geom_linerange(data = dhs_pfpr_summary, aes(x = pfpr, ymin = smal, ymax = smau), col = "#2e4057") +
+  geom_linerange(data = dhs_pfpr_summary, aes(y = sma_prevalence, xmin = pfprl, xmax = pfpru), col = "#2e4057") +
+  geom_point(data = dhs_pfpr_summary, aes(x = pfpr, y = sma_prevalence), col = "#2e4057") +
   xlab(expression(~italic(Pf)~Pr[2-10])) +
   ylab(expression(SMAPr[0.5-5])) + 
   facet_wrap(~ country, scales = "free") +
-  theme_bw()
+  theme_bw() +
+  theme(strip.background = element_rect(fill = NA))
+
+dhs_distance_fits <- ggplot() +
+  geom_line(data = dhs_distance_prediction, aes(x = distance, y = sma_prevalence, group = sample), alpha = 0.1, col = "#00798c") +
+  geom_line(data = dhs_distance_median_prediction, aes(x = distance, y = sma_prevalence), col = "#edae49", size = 1) +
+  geom_linerange(data = dhs_distance_summary, aes(x = distance, ymin = smal, ymax = smau), col = "#2e4057") +
+  geom_linerange(data = dhs_distance_summary, aes(y = sma_prevalence, xmin = distancel, xmax = distanceu), col = "#2e4057") +
+  geom_point(data = dhs_distance_summary, aes(x = distance, y = sma_prevalence), col = "#2e4057") +
+  xlab("Distance") +
+  ylab(expression(SMAPr[0.5-5])) + 
+  facet_wrap(~ country, scales = "free") +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = NA))
 ################################################################################
 
 ### Global plots of SMA predictions ############################################
-ggplot(pfpr_prediction, aes(x = pfpr, y = sma_prevalence, group = sample,
-                            alpha = alpha, col = col, lty = lty)) +
-  geom_line() +
-  scale_linetype_manual(values = c(1, 2, 1), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_colour_manual(values = c("red", "darkred", "black"), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_alpha(guide = "none") + 
+ggplot() +
+  geom_line(data = pfpr_prediction, aes(x = pfpr, y = sma_prevalence, group = sample), alpha = 0.1, col = "#00798c") +
+  geom_line(data = pfpr_median_prediction, aes(x = pfpr, y = sma_prevalence), col = "#edae49", size = 1) +
   xlab(expression(~italic(Pf)~Pr[2-10])) +
   ylab(expression(SMAPr[0.5-5])) + 
   theme_bw()
 
-ggplot(distance_prediction, aes(x = distance, y = sma_prevalence, group = sample,
-                                alpha = alpha, col = col, lty = lty)) +
-  geom_line() +
-  scale_linetype_manual(values = c(1, 2, 1), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_colour_manual(values = c("red", "darkred", "black"), labels = c("Median", "95% CrI", "draw"), name = "") +
-  scale_alpha(guide = "none") + 
+ggplot() +
+  geom_line(data = distance_prediction, aes(x = distance, y = sma_prevalence, group = sample), alpha = 0.1, col = "#00798c") +
+  geom_line(data = distance_median_prediction, aes(x = distance, y = sma_prevalence), col = "#edae49", size = 1) +
   xlab("Distance") +
   ylab(expression(SMAPr[0.5-5])) + 
   theme_bw()
@@ -262,35 +299,40 @@ ggplot(distance_prediction, aes(x = distance, y = sma_prevalence, group = sample
 
 ### Paton fit plots ############################################################
 paton_fit <- ggplot() +
-  geom_line(data = paton_prediction, aes(x = pfpr, y = hospital, group = sample,
-                alpha = alpha, col = col, lty = lty)) +
-  geom_line(data = paton_model_prediction, aes(x = pfpr, y = hospital), col = "green") +
-  geom_point(data = paton_summary, aes(x = pfpr, y = hospital), col = "dodgerblue") +
+  geom_line(data = paton_prediction, aes(x = pfpr, y = hospital, group = sample), alpha = 0.1, col = "#00798c") +
+  geom_line(data = paton_median_prediction, aes(x = pfpr, y = hospital), col = "#edae49", size = 1) +
+  geom_line(data = paton_model_prediction, aes(x = pfpr, y = hospital), col = "#d1495b") +
+  geom_point(data = paton_summary, aes(x = pfpr, y = hospital), col = "#2e4057") +
   scale_linetype_manual(values = c(1, 2, 1), labels = c("Median", "95% CrI", "draw"), name = "") +
   scale_colour_manual(values = c("red", "darkred", "black"), labels = c("Median", "95% CrI", "draw"), name = "") +
   scale_alpha(guide = "none") + 
-  facet_wrap(~country) +
-  theme_bw()
+  xlab(expression(~italic(Pf)~Pr[2-10])) +
+  ylab("Annual hospitalised incidence per 1000 children") +
+  facet_wrap(~ country) +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = NA))
 ################################################################################
 
 ### Hospitalisation plot #######################################################
-ggplot(hospital_comunity, aes(x = pfpr, y = inc, col = location, lty = lty,
-             alpha = alpha, group = interaction(sample, location))) +
-  geom_line() +
+ggplot() +
+  geom_line(data = hospital_comunity, aes(x = pfpr, y = inc, col = location, group = interaction(sample, location)), alpha = 0.1) +
+  geom_line(data = hospital_community_median, aes(x = pfpr, y = inc, col = location), size = 1) +
+  scale_color_manual(values = c("#00798c", "8d96a3")) +
   scale_linetype_manual(values = c(1, 2, 1), labels = c("Median", "95% CrI", "draw"), name = "") +
   scale_alpha(guide = "none") + 
   ylab("Annual incidence per 1000 children") +
   facet_wrap(~country, scales = "free_y") +
-  theme_bw()
+  theme_bw() +
+  scale_y_log10()
 
 ggplot(paton_prediction, aes(x = p_hosp)) +
-  geom_histogram(bins = 50, fill = "darkblue") +
+  geom_histogram(bins = 10, fill = "darkblue") +
   facet_wrap(~country, scales = "free_y") +
   theme_bw()
 ################################################################################
 
 ### Random effects #############################################################
-ggplot(country_parameters, aes(x = country, fill = country, y = country_capacity)) +
+ggplot(parameters, aes(x = country, fill = country, y = country_capacity)) +
   geom_hline(yintercept = 0, lty = 2) +
   geom_boxplot(coef = 100) +
   xlab("") + 
